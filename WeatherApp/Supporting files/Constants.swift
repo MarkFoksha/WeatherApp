b8e4d82f9afdb6cf27d5c627349bf04b49127325
Mark Foksha <mark.foksha1813@gmail.com>
2023-01-26 15:41:12 +0200

    Arranging the project look Move network code from VC to separate file Add new file for global constants

---
+++ WeatherApp/Supporting files/Constants.swift
@@ -0,0 +1,10 @@
+//
+//  Constants.swift
+//  WeatherApp
+//
+//  Created by Марк Фокша on 26.01.2023.
+//
+
+import Foundation
+
+let apiKey = "0f3d57bb1e33dea23387589015c80bca"
--- WeatherApp/View Controllers/ViewController.swift
+++ WeatherApp/View Controllers/ViewController.swift
@@ -14,6 +14,7 @@ class ViewController: UIViewController {
     @IBOutlet weak var temperatureLabel: UILabel!
     @IBOutlet weak var feelsLikeTemperatureLabel: UILabel!
     
+    let networkWeatherManager = NetworkWeatherManager()
     
     @IBAction func searchPressed(_ sender: UIButton) {
         self.presentSearchAlertController(withTitle: "Enter city name", message: nil, style: .alert)
@@ -21,8 +22,9 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
-    }
+        networkWeatherManager.fetchCurrentWeather(forCity: "London")
+    } 
+    
 
 
 }
